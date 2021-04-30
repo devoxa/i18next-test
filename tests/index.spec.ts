379b9de9be12f308d1ebdafa04de5acb0ad86ed8
queicherius <queicherius@gmail.com>
2021-04-30 12:13:45 +0000

    Add tests

---
+++ tests/index.spec.ts
@@ -0,0 +1,153 @@
+import { testLocaleFile } from '../src/index'
+
+jest.mock('colors/safe', () => ({
+  cyan: (x: string) => x,
+  green: (x: string) => x,
+  red: (x: string) => x,
+}))
+
+describe('i18next-test', () => {
+  it('passes for a valid locale file in the default language', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({ 'Sign in': 'Sign in' }),
+      locale: 'en',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual([])
+  })
+
+  it('passes for a valid locale file in the translated language', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({ 'Sign in': 'Anmelden' }),
+      locale: 'de',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual([])
+  })
+
+  it('errors when the locale file could not be parsed as JSON', () => {
+    const errors = testLocaleFile({
+      fileContent: `{`,
+      locale: 'de',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual(['File content could not be parsed as JSON'])
+  })
+
+  it('errors for keys that do not have a translation', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({ 'Sign in': '' }),
+      locale: 'en',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual(['"Sign in" does not have a translation'])
+  })
+
+  it('errors for keys that have a translation equal to the source language', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({ 'Sign in': 'Sign in' }),
+      locale: 'de',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual(['"Sign in" has a translation equal to the source language'])
+  })
+
+  it('passes for keys that have a translation equal to the source language in the default language', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({ 'Sign in': 'Sign in' }),
+      locale: 'en',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual([])
+  })
+
+  it('errors for keys that have mismatching component markers in the translation', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({
+        'Sign in <0>here</0>': 'Sign in <0>here</0>', // OK
+        'Sign in <1>here</1>': 'Sign in <2>here</2>', // FAIL (wrong name)
+        'Sign in <2>here</2>': 'Sign in here', // FAIL (missing)
+      }),
+      locale: 'en',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual([
+      `"Sign in <1>here</1>" has mismatching component markers in the translation
+Expected: ["</1>","<1>"]
+Received: ["</2>","<2>"]`,
+      `"Sign in <2>here</2>" has mismatching component markers in the translation
+Expected: ["</2>","<2>"]
+Received: []`,
+    ])
+  })
+
+  it('errors for keys that have mismatching interpolation markers in the translation', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({
+        'Sign in {{count1}}': 'Sign in {{count1}}', // OK
+        'Sign in {{count2}}': 'Sign in {{countt2}}', // FAIL (wrong name)
+        'Sign in {{count3}}': 'Sign in', // OK (missing)
+      }),
+      locale: 'en',
+      defaultLocale: 'en',
+      namespace: 'sign-in',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual([
+      `"Sign in {{count2}}" has mismatching interpolation markers in the translation
+Expected: ["{{count2}}"]
+Received: ["{{countt2}}"]`,
+    ])
+  })
+
+  // We use i18next-parser, which extracts unused keys into namespaces suffixed with `_old`
+  it('errors for keys that are tagged as removed from source code', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({
+        'Sign in': 'Anmelden',
+      }),
+      locale: 'de',
+      defaultLocale: 'en',
+      namespace: 'sign-in_old',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual([`"Sign in" is tagged as removed from source code`])
+  })
+
+  it('errors for keys that are missing an explicit namespace', () => {
+    const errors = testLocaleFile({
+      fileContent: JSON.stringify({
+        'Sign in': 'Sign in',
+      }),
+      locale: 'en',
+      defaultLocale: 'en',
+      namespace: 'undefined',
+      defaultNamespace: 'undefined',
+    })
+
+    expect(errors).toEqual([`"Sign in" is missing an explicit namespace`])
+  })
+})
